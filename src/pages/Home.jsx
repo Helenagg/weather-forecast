6b996018952a823712702925ca34e603aee32107
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2025-01-12 12:26:56 +0100

    search added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,40 @@
+import { Box, InputAdornment, TextField } from "@mui/material"
+import { useState } from "react"
+import SearchIcon from '@mui/icons-material/Search';
+
 const Home = () => {
+  const [city, setCity] = useState('')
+console.log('city', city);
+  const handleChange = (event) => {
+    setCity(event.target.value)
+  }
+
   return (
-    <div>Home</div>
+    <Box 
+      display='flex'
+      justifyContent='center'
+      alignItems='center'
+      marginTop={4}
+    >
+    <TextField 
+      label='Buscar ciudad'
+      variant='outlined'
+      value={city}
+      onChange={handleChange}
+      placeholder='Indica la ciudad que deseas buscar'
+      fullWidth
+      sx={{
+      width: 600,
+      }}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon />
+          </InputAdornment>
+        )
+      }}
+    />    
+    </Box>
   )
 }
 

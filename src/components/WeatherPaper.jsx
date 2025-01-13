1dfd08902b16e577000cb3ffefcf4e1af7322d61
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2025-01-13 09:04:54 +0100

    weather data completed

---
+++ src/components/WeatherPaper.jsx
@@ -0,0 +1,110 @@
+import {
+  Box,
+  Button,
+  Divider,
+  InputAdornment,
+  Paper,
+  TextField,
+  Typography,
+} from '@mui/material';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import ThermostatIcon from '@mui/icons-material/Thermostat';
+import { WiHumidity } from "react-icons/wi";
+import { GiPressureCooker } from "react-icons/gi";
+import WaterSharpIcon from '@mui/icons-material/WaterSharp';
+import AirSharpIcon from '@mui/icons-material/AirSharp';
+import { TbTemperatureFahrenheit } from "react-icons/tb";
+import { TbTemperatureCelsius } from "react-icons/tb";
+
+
+const WeatherPaper = ({ data, unit, toggleUnit }) => {
+    
+  return (
+    <>
+      <Box sx={{ px: 10 }}>
+        <Paper
+          elevation={3}
+          sx={{
+            boxShadow: (theme) => `0px 4px 10px ${theme.palette.primary.light}`,
+            mt: 2,
+            p: 2,
+          }}>
+          <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+            <TextField
+              variant='standard'
+              value={data.name}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    <LocationOnIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+            <Box
+              component='img'
+              src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+              alt={data.weather[0].description}
+              sx={{
+                width: 100,
+                height: 100,
+              }}
+            />
+          </Box>
+          <Box
+            component='section'
+            sx={{ display: 'flex', justifyContent: 'flex-start', mt: 2 }}>
+            <Typography
+              variant='h2'
+              sx={{ fontWeight: 'bold', color: 'primary.dark' }}>
+              <ThermostatIcon sx={{ fontSize: 50, pt: 1 }} />
+              {data.main.temp}
+              {unit === 'metric' ? <TbTemperatureCelsius /> : <TbTemperatureFahrenheit />}
+            </Typography>
+            <Button variant="contained" sx={{ mx: 2, my: 2 }} onClick={toggleUnit}>Cambiar a: {unit === 'metric' ? '\u00b0F' : '\u00b0C'}</Button>
+            <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'flex-end', ml: 'auto' }}>
+              <Typography variant='body' sx={{ color: 'primary.dark' }}>
+                Temperatura Máxima: {data.main.temp_max}
+              </Typography>
+              <Typography variant='body' sx={{ color: 'primary.dark' }}>
+                Temperatura Mínima: {data.main.temp_min}
+              </Typography>
+            </Box>
+          </Box>
+          <Divider />
+          <Box sx={{ display: 'flex', mt: 1}}>
+          <WiHumidity size={40}/>
+          <Typography variant='body' sx={{ mt: 1}}>
+            Humedad: {data.main.humidity}
+          </Typography>
+          </Box>
+          <Divider />
+          <Box sx={{ display: 'flex', mt: 2, mb: 1, mx: 1}}>
+          <GiPressureCooker size={25}/>
+          <Typography variant='body' sx={{ }}>
+            Presión:{' '}{data.main.pressure}
+          </Typography>
+          </Box>
+          <Divider />
+          <Box sx={{ display: 'flex', mt: 2, mb: 1, mx: 1}}>
+          <WaterSharpIcon />
+          <Typography variant='body' sx={{ }}>
+            Nivel del mar:{' '}{data.main.sea_level}
+          </Typography>
+          </Box>
+          <Divider />
+          <Box sx={{ display: 'flex', mt: 2, mx: 1}}>
+          <AirSharpIcon />
+          <Typography variant='body' sx={{ }}>
+            Velocidad del viento:{' '}{data.wind.speed}
+          </Typography>
+          </Box>
+        </Paper>
+      </Box>
+    </>
+  );
+};
+
+export default WeatherPaper;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,65 +1,82 @@
-import { Box, Button, InputAdornment, TextField } from "@mui/material"
-import { useEffect, useState } from "react"
+import { Box, Button, InputAdornment, TextField } from '@mui/material';
+import { useEffect, useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
-import { useDispatch, useSelector } from "react-redux";
-import { getWeatherByCity } from "../reducers/WeatherByCityReducer";
+import { useDispatch, useSelector } from 'react-redux';
+import { getWeatherByCity } from '../reducers/WeatherByCityReducer';
+import WeatherPaper from '../components/WeatherPaper';
+import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
 
 const Home = () => {
-  const [city, setCity] = useState('')
-  const { data, loading, error } = useSelector(({ weatherByCity }) => weatherByCity)
+  const [city, setCity] = useState('');
+  const [unit, setUnit] = useState('metric');
+  const { data, loading, error } = useSelector(
+    ({ weatherByCity }) => weatherByCity
+  );
   console.log('data', data);
+  console.log('unit', unit);
 
-  const dispatch = useDispatch()
-console.log('city', city);
+  const dispatch = useDispatch();
+  console.log('city', city);
   const handleChange = (event) => {
-    setCity(event.target.value)
-  }
+    setCity(event.target.value);
+  };
 
   const apiKey = 'e4cdbdef3ca74e57f737ca795b2e971d';
 
   const handleSearch = () => {
     if (city) {
-      dispatch(getWeatherByCity(city, apiKey))
+      dispatch(getWeatherByCity(city, unit, apiKey));
     }
-  }
+  };
+  const toggleUnit = () => {
+    const newUnit = unit === 'metric' ? 'imperial' : 'metric';
+    setUnit(unit === 'metric' ? 'imperial' : 'metric');
+    dispatch(getWeatherByCity(city, newUnit, apiKey));
+  };
 
   return (
-    <Box 
-      display='flex'
-      justifyContent='center'
-      alignItems='center'
-      marginTop={4}
-      gap={2}
-    >
-    <TextField 
-      label='Buscar ciudad'
-      variant='outlined'
-      value={city}
-      onChange={handleChange}
-      placeholder='Indica la ciudad que deseas buscar'
-      fullWidth
-      sx={{
-      width: 600,
-      }}
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <SearchIcon />
-          </InputAdornment>
-        )
-      }}
-    />
-    <Button
-        variant="contained" 
-        color="primary" 
-        sx={{ p: 1.5}}
-        onClick={handleSearch}
-        startIcon={<SearchIcon />}
-      >
-        Buscar
-      </Button>
-    </Box>
-  )
-}
+    <>
+      <Box
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+        marginTop={4}
+        gap={2}>
+        <TextField
+          label='Buscar ciudad'
+          variant='outlined'
+          value={city}
+          onChange={handleChange}
+          placeholder='Indica la ciudad que deseas buscar'
+          fullWidth
+          sx={{
+            width: 600,
+          }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>
+                <AddLocationAltIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button
+          variant='contained'
+          color='primary'
+          sx={{ p: 1.5 }}
+          onClick={handleSearch}
+          startIcon={<SearchIcon />}>
+          Buscar
+        </Button>
+      </Box>
+      {data && (
+      <Box>
+        <WeatherPaper data={data} unit={unit} toggleUnit={toggleUnit}/>
+      </Box>
 
-export default Home
+      )}
+    </>
+  );
+};
+
+export default Home;
